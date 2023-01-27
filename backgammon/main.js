ad93cb2dd22c637616c9162bd64294a946c5162c
andrejbojko <AndreyBoyko@hollandandbarrett.com>
2023-01-28 03:26:22 +0400

    fix connection closing

--- backgammon/main.js
+++ backgammon/main.js
@@ -86,3 +86,8 @@ handleData = function(data) {
 generateConnectionLink = function(peerID) {
     return window.location.origin + window.location.pathname + "#" + peerID;
 }
+
+$(window).unload(function() {
+    if(connection) connection.close();
+
+})
